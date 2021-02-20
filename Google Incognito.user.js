1de10819e8a436ab694aa5c98d872d1bb0f3b668
kevingrillet <kevin.grillet@gmail.com>
2021-02-20 10:30:16 +0100

    Create Google Incognito.user.js

---
+++ Google Incognito.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          Google Incognito
+// @namespace     https://github.com/kevingrillet
+// @author        Kevin GRILLET
+// @description   Add banner to innactive github
+// @copyright     https://github.com/kevingrillet
+// @license       GPL-3.0 License
+// @version       1.2
+
+// @homepageURL   https://github.com/kevingrillet/Userscripts/
+// @supportURL    https://github.com/kevingrillet/Userscripts/issues
+// @downloadURL   https://github.com/kevingrillet/Userscripts/raw/main/Google%20Incognito.user.js
+// @updateURL     https://raw.githubusercontent.com/kevingrillet/Userscripts/main/Google%20Incognito.user.js
+
+// @match         *://github.com/*/*
+// @run-at        document-end
+// ==/UserScript==
