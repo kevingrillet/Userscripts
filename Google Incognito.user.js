cc92b2d03f746d9181f23252a4b42510104c517d
Kévin GRILLET <kevin.grillet@gmail.com>
2021-02-20 21:57:37 +0100

    Update Google Incognito.user.js

--- Google Incognito.user.js
+++ Google Incognito.user.js
@@ -5,13 +5,19 @@
 // @description   Add banner to innactive github
 // @copyright     https://github.com/kevingrillet
 // @license       GPL-3.0 License
-// @version       1.2
+// @version       0.1
 
 // @homepageURL   https://github.com/kevingrillet/Userscripts/
 // @supportURL    https://github.com/kevingrillet/Userscripts/issues
 // @downloadURL   https://github.com/kevingrillet/Userscripts/raw/main/Google%20Incognito.user.js
 // @updateURL     https://raw.githubusercontent.com/kevingrillet/Userscripts/main/Google%20Incognito.user.js
 
-// @match         *://github.com/*/*
+// @match         *
 // @run-at        document-end
 // ==/UserScript==
+
+document.querySelectorAll('a').forEach(el => {
+    if (el.href.test('.*docs\.google\.com.*')) {
+        el.onclick = `javascript:chrome.windows.create({"url": ${el.href}, "incognito": true}); return false;`;
+    }
+});
