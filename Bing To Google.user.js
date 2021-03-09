af7345d6ca749ca56f956b08faf5ce2796ee8175
kevingrillet <kevin.grillet@gmail.com>
2021-03-09 13:38:13 +0100

    Update Bing To Google.user.js

--- Bing To Google.user.js
+++ Bing To Google.user.js
@@ -5,7 +5,7 @@
 // @description   Redirect the search to google
 // @copyright     https://github.com/kevingrillet
 // @license       GPL-3.0 License
-// @version       1.0
+// @version       1.1
 
 // @homepageURL   https://github.com/kevingrillet/Userscripts/
 // @supportURL    https://github.com/kevingrillet/Userscripts/issues
@@ -13,11 +13,34 @@
 // @updateURL     https://raw.githubusercontent.com/kevingrillet/Userscripts/main/Bing%20To%20Google.user.js
 
 // @match         *://*.bing.com/search?*
+// @grant         GM_getValue
+// @grant         GM_setValue
 // @run-at        document-end
 // ==/UserScript==
 
 
 // **************************************************
+// **********   C A N   B E   E D I T E D  **********
+// **************************************************
+var doReward = false; //Launch 30 consecutives searches for easy 90Points / day
+
+
+// **************************************************
 // **********         S C R I P T          **********
 // **************************************************
-window.location.assign("https://google.com/search?"+document.URL.match(/q\=[^&]*/));
+if (doReward && (GM_getValue('dayDone', null) != new Date().getDay())) {
+    GM_setValue('dayDone', new Date().getDay());
+    GM_setValue('search', 0);
+    GM_setValue('toSearch', document.URL.match(/q\=[^&]*/));
+}
+
+if (!doReward || GM_getValue('search', 31) > 30) {
+    window.location.assign(`https://google.com/search?${document.URL.match(/q\=[^&]*/)}`)
+} else {
+    if (GM_getValue('search') == 30) {
+        window.location.assign(`https://www.bing.com/search?${GM_getValue('toSearch')}`);
+    } else {
+        window.location.assign(`https://www.bing.com/search?q=Reward${GM_getValue('search')}`);
+    }
+    GM_setValue('search', GM_getValue('search') + 1);
+}
