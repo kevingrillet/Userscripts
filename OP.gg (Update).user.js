42fdf66c7c1580a7b1af694b2e68cced60d0d876
kevingrillet <kevin.grillet@gmail.com>
2021-03-10 12:12:29 +0100

    Create OP.gg (Update).user.js

---
+++ OP.gg (Update).user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name          OP.gg (Update)
+// @namespace     https://github.com/kevingrillet
+// @author        Kevin GRILLET
+// @description   Auto-Update profile
+// @copyright     https://github.com/kevingrillet
+// @license       GPL-3.0 License
+// @version       1.0
+
+// @homepageURL   https://github.com/kevingrillet/Userscripts/
+// @supportURL    https://github.com/kevingrillet/Userscripts/issues
+// @downloadURL   https://github.com/kevingrillet/Userscripts/raw/main/OP.gg%20(Update).user.js
+// @updateURL     https://raw.githubusercontent.com/kevingrillet/Userscripts/main/OP.gg%20(Update).user.js
+
+// @match         *://euw.op.gg/summoner/userName=*
+// @run-at        document-end
+// ==/UserScript==
+
+
+// **************************************************
+// **********   C A N   B E   E D I T E D  **********
+// **************************************************
+var minutes = 10, // number of minutes before auto refresh
+    cleanUI = true; // remove crap
+
+
+// **************************************************
+// **********             U I              **********
+// **************************************************
+function removeCrap() {
+    document.querySelectorAll('.image-banner').forEach(e => e.remove());
+    document.querySelectorAll('.vm-placement').forEach(e => e.remove());
+    document.querySelectorAll('.life-owner').forEach(e => e.remove());
+    document.querySelectorAll('.vm-skin').forEach(e => e.remove());
+}
+
+
+// **************************************************
+// **********       L I S T E N E R        **********
+// **************************************************
+window.addEventListener('load', function () {
+    if (cleanUI) removeCrap();
+
+    if ((new Date() - new Date(document.querySelector('._timeago').title)) / (1000 * 60) >= minutes) {
+        document.querySelector('#SummonerRefreshButton').click();
+    }
+});
