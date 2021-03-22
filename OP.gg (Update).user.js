3315d5f9bbdb4e7fea0cef3a855f24072d11c984
kevingrillet <kevin.grillet@gmail.com>
2021-03-22 12:56:53 +0100

    Update OP.gg (Update).user.js

--- OP.gg (Update).user.js
+++ OP.gg (Update).user.js
@@ -5,7 +5,7 @@
 // @description   Auto-Update profile
 // @copyright     https://github.com/kevingrillet
 // @license       GPL-3.0 License
-// @version       1.0
+// @version       1.1
 
 // @homepageURL   https://github.com/kevingrillet/Userscripts/
 // @supportURL    https://github.com/kevingrillet/Userscripts/issues
@@ -33,6 +33,42 @@ function removeCrap() {
     document.querySelectorAll('.life-owner').forEach(e => { e.style.display = "none" });
     document.querySelectorAll('.vm-skin').forEach(e => { e.style.display = "none" });
 }
+
+
+// **************************************************
+// **********       O B S E R V E R        **********
+// **************************************************
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+function openAll(){
+    document.querySelectorAll(':scope .SpectateSummoner .SummonerName a').forEach(s => {
+        if (s.innerHTML != document.querySelector('.Name').innerHTML) s.click()
+    });
+}
+
+function addButtonOpenAll() {
+    let el = document.querySelector('.SpectatorButtons').appendChild(document.createElement('button'));
+    el.classList.add('Button', 'SemiRound', 'RealWhite');
+    el.innerHTML = `
+        <i class="__spSite __spSite-157"></i>
+        Open all players profile
+    `;
+    el.onclick = function () { openAll(); };
+}
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+        for(const node of mutation.addedNodes) {
+            if (node.classList && node.classList.contains('SpectatorButtons')) addButtonOpenAll();
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+// Start observing the target node for configured mutations
+observer.observe(document.querySelector('.summonerLayout-spectator'), { attributes: true, childList: true, subtree: true });
 
 
 // **************************************************
