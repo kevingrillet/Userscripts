3270ccd445484e1f4346fc9f2164ac56a266950b
Kévin GRILLET <kevin@MacBook-Pro-de-Kevin.local>
2021-04-04 08:49:54 +0200

    Update Github Inactive.user.js

--- Github Inactive.user.js
+++ Github Inactive.user.js
@@ -5,7 +5,7 @@
 // @description   Add banner to innactive github
 // @copyright     https://github.com/kevingrillet
 // @license       GPL-3.0 License
-// @version       1.2
+// @version       1.3
 
 // @homepageURL   https://github.com/kevingrillet/Userscripts/
 // @supportURL    https://github.com/kevingrillet/Userscripts/issues
@@ -20,20 +20,31 @@
 // **************************************************
 // **********   C A N   B E   E D I T E D  **********
 // **************************************************
-var days = 365 / 2;
+var days = 365 / 2
+    bLightMode = true;
 
 
 // **************************************************
 // **********         S C R I P T          **********
 // **************************************************
+function addDiv(){
+    let elDiv = document.createElement('div');
+    if (bLightMode) {
+        // could use flash-warn instead, but less flashy.
+        elDiv.classList.add('flash', 'flash-error', 'flash-full', 'border-top-0', 'text-center', 'text-bold', 'py-2');
+        elDiv.innerHTML = 'WARNING: This repo is pretty old ;)';
+    } else {
+        elDiv.setAttribute('style', 'display: flex; height: 50px; width: 100%; background-color: tomato; color: white; font-size: 2rem; font-family: consolas; text-align: center;');
+        elDiv.innerHTML = '<p style="width: 100%">WARNING: This repo is pretty old ;) </p>';
+    }
+    document.querySelector('#js-repo-pjax-container').prepend(elDiv);
+}
+
 window.addEventListener('load', function () {
     let myInterval = setInterval(function () {
         if (document.querySelector('relative-time')) {
             if (Date.now() - new Date(document.querySelector('relative-time').date) > (days * 1000 * 60 * 60 * 24)) {
-                let elDiv = document.createElement("div");
-                elDiv.setAttribute('style', 'display: flex; height: 50px; width: 100%; background-color: tomato; color: white; font-size: 2rem; font-family: consolas; text-align: center;');
-                elDiv.innerHTML = '<p style="width: 100%">WARNING: This repo is pretty old ;) </p>';
-                document.querySelector('#js-repo-pjax-container').prepend(elDiv);
+                addDiv();
             }
             clearInterval(myInterval);
             myInterval = null;
