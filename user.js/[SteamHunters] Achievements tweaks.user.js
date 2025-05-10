bf28481763d9afce997e7fccb1eb4250f73ebe10
kevingrillet <kevin.grillet@gmail.com>
2025-05-10 11:16:08 +0200

    Add button save

--- user.js/[SteamHunters] Achievements tweaks.user.js
+++ user.js/[SteamHunters] Achievements tweaks.user.js
@@ -7,7 +7,7 @@
 // @license       GPL-3.0 License
 // @tag           kevingrillet
 // @tag           steamhunters.com
-// @version       1.0.2
+// @version       1.0.3
 
 // @homepageURL   https://github.com/kevingrillet/Userscripts/
 // @supportURL    https://github.com/kevingrillet/Userscripts/issues
@@ -16,18 +16,20 @@
 
 // @match         https://steamhunters.com/id/*/apps/*/achievements
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=steamhunters.com
-// @grant         none
+// @grant         GM_getValue
+// @grant         GM_setValue
 // @run-at        document-end
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    let isFiltered = false;
+    let isFiltered = GM_getValue('isFiltered', false);
 
-    function toggleVisibility() {
-        isFiltered = !isFiltered;
+    function applyFilter() {
         const button = document.getElementById('toggle-visibility-btn');
+        if (!button) return;
+
         const icon = button.querySelector('i');
 
         // Update button appearance
@@ -54,17 +56,44 @@
         });
     }
 
-    const buttonTemplate = `
-        <button id="toggle-visibility-btn" type="button" class="btn btn-default btn-xs">
-            <i class="fa fa-filter"></i><span> Hide Completed</span>
-        </button>
-    `.trim();
-
-    const spanContainer = document.querySelector('span.pull-right');
-    if (spanContainer) {
-        spanContainer.insertAdjacentHTML('beforeend', buttonTemplate);
-        document.getElementById('toggle-visibility-btn').addEventListener('click', toggleVisibility);
-    } else {
-        console.error('Unable to find <span class="pull-right">');
+    function toggleVisibility() {
+        isFiltered = !isFiltered;
+        GM_setValue('isFiltered', isFiltered);
+        applyFilter();
     }
+
+    function initButton() {
+        const buttonTemplate = `
+            <button id="toggle-visibility-btn" type="button" class="btn btn-default btn-xs">
+                <i class="fa fa-filter"></i><span> Hide Completed</span>
+            </button>
+        `.trim();
+
+        const spanContainer = document.querySelector('span.pull-right');
+        if (spanContainer && !document.getElementById('toggle-visibility-btn')) {
+            spanContainer.insertAdjacentHTML('beforeend', buttonTemplate);
+            document.getElementById('toggle-visibility-btn').addEventListener('click', toggleVisibility);
+            applyFilter();
+        }
+    }
+
+    // Observer for dynamic content
+    const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                initButton();
+                applyFilter();
+            }
+        }
+    });
+
+    // Start observing
+    const targetNode = document.querySelector('.achievements');
+    if (targetNode) {
+        observer.observe(targetNode, { childList: true, subtree: true });
+    }
+
+    // Initial setup
+    initButton();
+    applyFilter();
 })();
